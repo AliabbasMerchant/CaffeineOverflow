c1bfae25a8cf794f9e378d12fcbc7826af637b79
AliabbasMerchant <merchantaliabbas@gmail.com>
2018-10-05 17:57:07 +0530

    Card Changes

--- app/src/main/java/IssueCard.java
+++ app/src/main/java/IssueCard.java
@@ -12,8 +12,9 @@ import org.json.JSONObject;
 
 public class IssueCard extends ConstraintLayout {
     JSONObject data;
-    TextView issue_title, issue_desc, upvote_count, comment_count, location;
+    TextView issue_title, upvote_count, comment_count, location, issue_category;
     ImageView issue_image;
+    TextView issue_desc;
     public IssueCard(Context context) {
         super(context);
         LayoutInflater.from(getContext()).inflate(R.layout.issue_card, this, true);
@@ -27,22 +28,23 @@ public class IssueCard extends ConstraintLayout {
     public void inflate_data() {
         issue_title = findViewById(R.id.issue_title);
         issue_image = findViewById(R.id.issue_image);
-        issue_desc = findViewById(R.id.issue_desc);
+        issue_category = findViewById(R.id.issue_category);
+//        issue_desc = findViewById(R.id.issue_desc);
         upvote_count = findViewById(R.id.upvote_count);
         location = findViewById(R.id.location);
         comment_count = findViewById(R.id.comment_count);
 
         try {
             issue_title.setText(data.getString("Title"));
-            issue_desc.setText(data.getString("Desc"));
-            // TODO wanna add desc or not??
+//            issue_desc.setText(data.getString("Desc"));
             Picasso.get()
                     .load(data.getString("ImageURL"))
-                    .resize(50, 50)
+                    .resize(getWidth(), 100)
                     .centerCrop()
                     .into(issue_image);
             location.setText(data.getString("Location"));
-
+            comment_count.setText(data.getString("CommentCount"));
+            upvote_count.setText(data.getString("UpvoteCount"));
 
         } catch (JSONException e) {
             e.printStackTrace();
