408ee5605227e2f319ebf6e05a417d2ecf669abd
Sanidhya <sanidhyarsharma@gmail.com>
2018-10-05 16:35:04 +0530

    Filled navigation bar

--- app/src/main/java/com/example/sanidhya/m_xpress/MainActivity.java
+++ app/src/main/java/com/example/sanidhya/m_xpress/MainActivity.java
@@ -1,13 +1,71 @@
 package com.example.sanidhya.m_xpress;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private NavigationView sideNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Custom action bar to adhere to material design
+        ActionBar actionbar = getSupportActionBar();
+        if(actionbar!=null)
+        {
+            actionbar.setDisplayHomeAsUpEnabled(true);
+            actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+        //Navigation Drawer
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        sideNavigation = findViewById(R.id.nav_view);
+
+        sideNavigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if(!menuItem.isChecked()){
+                    menuItem.setChecked(true);
+                    mDrawerLayout.closeDrawers();
+                    switchFragments(menuItem.getItemId());
+                }
+                else{
+                    mDrawerLayout.closeDrawers();
+                }
+                return true;
+            }
+        });
+    }
+
+    private void switchFragments(int id){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        switch(id){
+//            case R.id.drawer_layout: return new PostsFragment();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
